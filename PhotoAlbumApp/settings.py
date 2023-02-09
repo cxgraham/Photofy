98d85e07b4192565c0d13f904773c8734b50c63b
Christian Graham <christian.graham18@gmail.com>
2023-02-08 20:59:55 -0600

    fixes

--- PhotoAlbumApp/settings.py
+++ PhotoAlbumApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
